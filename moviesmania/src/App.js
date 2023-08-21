6b39ea6cc915ef82cfda780ce072c97f25b62dec
meet_hansalia <meethansalia99@gmail.com>
2023-08-21 13:14:52 +0530

    initial setup

---
+++ moviesmania/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>header</header>
+      <h1>hi</h1>
+      <footer>footer</footer>
+    </div>
+  );
+}
+
+export default App;
