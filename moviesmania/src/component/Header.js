6ca8ee932c7714382cb220a3327c16ecbc6edd75
meet_hansalia <meethansalia99@gmail.com>
2023-08-23 16:00:50 +0530

    search bar anabled

--- moviesmania/src/component/Header.js
+++ moviesmania/src/component/Header.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import logo from "../assets/logo.png"
 
 export const Header = () => {
   const [hidden, setHidden]= useState(true)
   const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem("darkMode")) || true)
+  const navigate = useNavigate()
 
   useEffect(()=>{
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
@@ -17,6 +18,16 @@ export const Header = () => {
     
   },[darkMode])
 
+  const submitHandler = (event)=>{
+    event.preventDefault()
+    const queryTerm = event.target.search.value;
+    event.target.reset()
+
+    return(
+      navigate(`/search?q=${queryTerm}`)
+    )
+  }
+
   const activeClass ="text-base block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 end"
   const inActiveClass ="text-base block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
   
@@ -24,10 +35,10 @@ export const Header = () => {
   return (
     <div>
       <header>        
-        <nav className="bg-white border-b-2 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900 dark:border-b-1 dark:border-gray-900">
+        <nav className="bg-white border-b-2 border-gray-200 px-2 sm:px-4 py-2 dark:bg-gray-900 dark:border-b-1 dark:border-gray-900">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <NavLink to="/" className="flex items-center">
-                <img src={logo} className="h-8 mr-3" alt="Cinemate Logo" />
+                <img src={logo} className="mr-2 h-6 sm:h-9" alt="Cinemate Logo" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Cinemate</span>
             </NavLink>
 
@@ -48,14 +59,16 @@ export const Header = () => {
                   </svg>
                   <span className="sr-only">Search icon</span>
                 </div>
-                <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." autoComplete="off"/>
+                <form onSubmit={submitHandler}>
+                  <input type="text" name='search' id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." autoComplete="off"/>
+                </form>
               </div>
               <button onClick={() => setHidden(!hidden)} data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
                   <span className="sr-only">Open main menu</span>
                   <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                       <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
                   </svg>
-              </button>
+              </button> 
             </div>
 
             <div className={`items-center justify-between ${hidden ? "hidden": "" } w-full md:flex md:w-auto md:order-1`} id="nav-links">
@@ -65,7 +78,9 @@ export const Header = () => {
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                   </svg>
                 </div>
-                <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." autoComplete="off"/>
+                <form onSubmit={submitHandler}>
+                  <input type="text" id="search-navbar" name='search' className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." autoComplete="off"/>
+                </form>
               </div>
               <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
--- moviesmania/src/hooks/useFetch.js
+++ moviesmania/src/hooks/useFetch.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react"
 
 
-export const useFetch = (apiPath) => {
+export const useFetch = (apiPath, queryTerm="") => {
     const [data, setData]= useState([])
-    const url=`https://api.themoviedb.org/3/${apiPath}?api_key=55ba11d7f869e89608994ad683b819e1&language=en-US&page=1`
+    const url=`https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&query=${queryTerm}`
 
     useEffect(()=>{
         async function fetchMovies(){
